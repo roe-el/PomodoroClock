324d5694d3b95c45c0d86876637aeed84001f118
roe-el <rogar28@gmail.com>
2017-06-22 00:29:03 -0500

    Add event listeners for play and reset buttons.

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,24 @@
 $(document).ready(function() {
     var breakCount = 5;
     var sessionCount = 25;
+    var breakHistroy = 0;
+    var sessionHistory = 0;
     /*Break Add/Subtract button event listeners*/
     $('.break-add').on('click', function() {
         breakCount += 1;
         $('.break-num').text(breakCount);
-        console.log(breakCount);
     })
     $('.break-sub').on('click', function() {
-            if (breakCount > 1) {
-                breakCount -= 1;
-                $('.break-num').text(breakCount);
-            }
-        })
-        /*Session Add/Subtract button even listeners*/
+        if (breakCount > 1) {
+            breakCount -= 1;
+            $('.break-num').text(breakCount);
+        }
+    })
+
+    /*Session Add/Subtract button event listeners*/
     $('.session-add').on('click', function() {
         sessionCount += 1;
         $('.session-num').text(sessionCount);
-        console.log(sessionCount);
     })
     $('.session-sub').on('click', function() {
         if (sessionCount > 1) {
@@ -26,4 +27,15 @@ $(document).ready(function() {
         }
     })
 
+    /*Play and Reset button event listeners*/
+    $('.play-btn').on('click', function() {
+        breakHistory = breakCount;
+        sessionHistory = sessionCount;
+    })
+    $('.reset-btn').on('click', function() {
+        breakCount = breakHistory;
+        $('.break-num').text(breakCount);
+        sessionCount = sessionHistory;
+        $('.session-num').text(sessionCount);
+    })
 });
