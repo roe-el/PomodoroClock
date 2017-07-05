89a92a5e8ed86d2ab7f3cba752d4b8803b7e8f1e
roe-el <rogar28@gmail.com>
2017-07-05 18:01:14 -0500

    Add ding object. Add comments and include change of font color for colon.

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     var sessionHistory = 0;
     var sessionTime = 0;
     var breakTime = breakCount * 60;
-
+    var ding = new Audio("ding.mp3");
     /*Break Add/Subtract button event listeners*/
     $('.break-add').on('click', function () {
         breakCount += 1;
@@ -39,61 +39,78 @@ $(document).ready(function () {
         sessionTime = sessionCount * 60;
         breakTime = breakCount * 60;
         console.log(sessionTime);
-        if($('#min-zero,#min-time,#zero,#sec-time').hasClass('red')){
-           $('#min-zero,#min-time,#zero,#sec-time').removeClass('red'); 
+        //Change color of text when new timer is started.
+        if($('#min-zero,#min-time,#zero,#sec-time,#colon').hasClass('red')){
+           $('#min-zero,#min-time,#zero,#sec-time,#colon').removeClass('red'); 
         }
-        $('#min-zero,#min-time,#zero,#sec-time').addClass('blue');
+        //Change color of text when session timer begins.
+        $('#min-zero,#min-time,#zero,#sec-time,#colon').addClass('blue');
         sessionTimer.start(sessionTime).on('end', function () {
-            $('#min-zero,#min-time,#zero,#sec-time').removeClass('blue');
-            $('#min-zero,#min-time,#zero,#sec-time').addClass('red');
+            //Change color of text when session timer ends and break begins.
+            $('#min-zero,#min-time,#zero,#sec-time,#colon').removeClass('blue');
+            $('#min-zero,#min-time,#zero,#sec-time,#colon').addClass('red');
+            ding.play();
             breakTimer.start(breakTime).on('end', function () {
+                //Change text back to 00:00:00 at end of break session.
                 $('#min-zero,#min-time,#zero,#sec-time').text('0');
+                ding.play();
                 console.log('Done');
             });
         });
     });
+    //Reset session and break history back to last used numbers.
     $('.reset-btn').on('click', function () {
         breakCount = breakHistory;
         $('.break-num').text(breakCount);
         sessionCount = sessionHistory;
         $('.session-num').text(sessionCount);
     })
+    //Creates object for breakTimer with keys for different options
     var breakTimer = new Timer({
         tick: 1,
         ontick: function (sec) { console.log('interval', sec);
+        //Sets zero if minutes is under 10
         if((Math.floor(sec / 60000)) < 10){
             $('#min-zero').text('0');
         }
         else{
             $('#min-zero').text('');
         }
+        //Sets minute time
         $('#min-time').text(Math.floor(sec/60000));
+        //If statements for zero on second timer
         if(((sec / 1000) % 60) < 9){
             $('#zero').text('0');
         }
         else{
             $('#zero').text('');
         }
+        //Seting seconds on the timer
         $('#sec-time').text(Math.round(sec / 1000) % 60); },
         onstart: function() { console.log('timer started');
          }
     });
+    //Creates object for sessionTimer with keys for different options
     var sessionTimer = new Timer({
         tick: 1,
         ontick: function (sec) { console.log('interval', sec);
+        //Sets zero if minutes is under 10
         if((Math.floor(sec / 60000)) < 10){
             $('#min-zero').text('0');
         }
         else{
             $('#min-zero').text('');
         }
+        //Sets minute time
         $('#min-time').text(Math.floor(sec/60000));
+        //If statements for zero on second timer
         if(((sec / 1000) % 60) < 9){
             $('#zero').text('0');
         }
         else{
             $('#zero').text('');
         }
+        //Seting seconds on the timer
         $('#sec-time').text(Math.round(sec / 1000) % 60); },
         onstart: function () {
             console.log('timer started');}
